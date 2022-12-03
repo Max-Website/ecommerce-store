b7dfac2a01af02dd827eabb32dc5991c39f0d68e
sam-lalli <2sam.lalli@gmail.com>
2022-12-03 14:00:19 -0700

    Product page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { ProductPage } from '../components/ProductPage'
 
 export default function Home() {
-  return (
+  return (<>
         <h1 className="text-3xl font-bold underline">
           Hello world!
         </h1>
+        <ProductPage classification={'Rings'} />
+        </>
       )
     }
