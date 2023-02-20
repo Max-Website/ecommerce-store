c3382f83ce52e51be1bbd06f298e011beb3a7b2f
Luis PM <luisp237@gmail.com>
2023-02-20 16:00:54 -0500

    changes

--- components/homepage/InfoBox.jsx
+++ components/homepage/InfoBox.jsx
@@ -5,10 +5,12 @@ import InfoItem from "./InfoItem";
 const InfoBox = () => {
   return (
     <div className="flex flex-wrap justify-center items-center w-full">
+      <div className="w-full max-w-6xl flex justify-start items-center">
         <InfoItem title={'Follow Us On Instagram'} desc={'Stay up to date with out release'} faIcon={faSearch} />
         <InfoItem title={'Fast Shipping'} desc={'Stay up to date with out release'} faIcon={faSearch} />
         <InfoItem title={'Need Assistance'} desc={'Stay up to date with out release'} faIcon={faSearch} />
         <InfoItem title={'View Return Policy'} desc={'Stay up to date with out release'} faIcon={faSearch} />
+      </div>
     </div>
   );
 };
